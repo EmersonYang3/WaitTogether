5aa44922e6d8d87376b07a3f146fd79612e2ac19
EmersonYang3 <emersonyang28@gmail.com>
2024-12-31 21:19:41 -0500

    Test (yes, again)

--- netlify/functions/visitor/visitor.mjs
+++ netlify/functions/visitor/visitor.mjs
@@ -12,58 +12,25 @@ const client = new MongoClient(uri, {
 });
 
 exports.handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
   try {
     console.time("MongoDB Connection");
     await client.connect();
     console.timeEnd("MongoDB Connection");
-    const database = client.db("UserData");
-    const collection = database.collection("UserStats");
 
-    let response;
-
-    if (event.httpMethod === "POST") {
-      console.time("POST Request");
-      await collection.updateMany(
-        {},
-        { $inc: { LiveUsers: 1, TotalUsers: 1 } }
-      );
-      response = {
-        statusCode: 200,
-        body: JSON.stringify({ message: "User added to waitlist" }),
-      };
-      console.timeEnd("POST Request");
-    } else if (event.httpMethod === "DELETE") {
-      console.time("DELETE Request");
-      await collection.updateMany({}, { $inc: { LiveUsers: -1 } });
-      response = {
-        statusCode: 200,
-        body: JSON.stringify({ message: "User removed from waitlist" }),
-      };
-      console.timeEnd("DELETE Request");
-    } else if (event.httpMethod === "GET") {
-      console.time("GET Request");
-      const document = await collection.findOne({});
-      response = {
-        statusCode: 200,
-        body: JSON.stringify({
-          LiveUsers: document.LiveUsers,
-          TotalUsers: document.TotalUsers,
-        }),
-      };
-      console.timeEnd("GET Request");
-    } else {
-      response = {
-        statusCode: 405,
-        body: "Method Not Allowed",
-      };
-    }
-
-    return response;
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Connected to MongoDB successfully!" }),
+    };
   } catch (error) {
     console.error("Error:", error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: "Internal Server Error" }),
+      body: JSON.stringify({
+        message: "Internal Server Error",
+        error: error.message,
+      }),
     };
   } finally {
     console.time("MongoDB Disconnection");
