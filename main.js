d1a28dab65e451e42755688780c184db541d77ad
EmersonYang3 <emersonyang28@gmail.com>
2024-12-31 21:36:59 -0500

    Fireworks + negative time fix

--- main.js
+++ main.js
@@ -5,53 +5,90 @@ document.addEventListener("DOMContentLoaded", () => {
   const secondsEl = document.getElementById("seconds");
   const visitorsEl = document.querySelector(".stats-container p");
 
-  const countdown = () => {
-    const newYear = new Date("January 1, 2025 00:00:00").getTime();
+  const duration = 15 * 1000;
+  const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  function triggerFireworks() {
+    const animationEnd = Date.now() + duration;
+    const interval = setInterval(function () {
+      const timeLeft = animationEnd - Date.now();
+
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+
+      const particleCount = 50 * (timeLeft / duration);
+
+      confetti(
+        Object.assign({}, defaults, {
+          particleCount,
+          origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+        })
+      );
+      confetti(
+        Object.assign({}, defaults, {
+          particleCount,
+          origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+        })
+      );
+    }, 250);
+  }
+
+  const countdown = (endTime) => {
     const now = new Date().getTime();
-    const distance = newYear - now;
+    const distance = endTime - now;
 
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(
-      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    if (distance >= 0) {
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    daysEl.textContent = days;
-    hoursEl.textContent = hours;
-    minutesEl.textContent = minutes;
-    secondsEl.textContent = seconds;
-
-    if (distance < 0) {
+      daysEl.textContent = days;
+      hoursEl.textContent = hours;
+      minutesEl.textContent = minutes;
+      secondsEl.textContent = seconds;
+    } else {
       clearInterval(interval);
       document.getElementById("headline").textContent = "Happy New Year!";
+      triggerFireworks();
     }
   };
 
   const updateVisitors = async () => {
     try {
       const response = await fetch("/.netlify/functions/visitor");
-
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
-
       const data = await response.json();
-      visitorsEl.textContent = `Current Waiters: ${data.LiveUsers}`;
+      visitorsEl.textContent = `Active Participants: ${data.LiveUsers}`;
     } catch (error) {
       console.error("Error fetching visitor count:", error);
-
       const responseText = await response.text();
       console.log("Response Text:", responseText);
     }
   };
 
-  setInterval(updateVisitors, 60000);
+  const now = new Date();
+  const currentYear = now.getFullYear();
+  const newYearDate = new Date(
+    `January 1, ${currentYear + 1} 00:00:00`
+  ).getTime();
 
-  countdown();
+  countdown(newYearDate);
   updateVisitors();
 
-  const interval = setInterval(countdown, 1000);
+  const interval = setInterval(() => {
+    countdown(newYearDate);
+    updateVisitors();
+  }, 1000);
 
   window.addEventListener("load", async () => {
     try {
